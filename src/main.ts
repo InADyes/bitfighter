9f5af97e2d0eb8f6b124b5f6fd88b9569ce2051d
maxwell burson <maxwellburson@gmail.com>
2017-08-22 11:30:20 -0700

    saving progress

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,58 @@
-function greeter(person: string) {
-    return "Hello " + person;
+
+function newChallenger() {
+    var challengerName = document.getElementById("challenger-name")
+    if (challengerName == null) {
+        console.error("challengerName is empty");
+        return;
+    }
+    var challengerStat = document.getElementById("challenger-stat")
+    if (challengerStat == null) {
+        console.error("challengerStat is empty");
+        return;
+    }
 }
 
-var user = [0, 1, 2];
+class Game {
+    challenger: Champion | null;
+    champion: Champion | null;
+    queue: Array<Champion> = [];
+    newChallenger(challenger: Champion) {
+        this.queue.push(challenger);
+    }
+    newFight() {
+
+    }
+}
+
+class Champion {
+    name: string = "";
+    status: Status;
+    constructor(name: string, status: Status) {
+        this.name = name;
+        this.status = status
+    }
+    toString() {
+        var value = `challenger: ${this.name}\n`;
+        for (let key in this.status) {
+            console.log(`key? ${key}`)
+        }
+    }
+}
+
+class Status {
+    health: number;
+}
 
 window.onload = function() {
     // var canvas = document.getElementById("game");
     // var ctx = canvas.getContext("2d")
     // ctx.fillStyle = "FF0000";
     // ctx.fillRect(0, 0, 150, 75);
-    document.body.innerHTML = greeter("yo")
+    //document.body.innerHTML = greeter("yo")
+    var newChallengerButton = document.getElementById("new-challenger")
+    if (newChallengerButton == null) {
+        console.error("no new-challenger button found");
+        return;
+    }
+    newChallengerButton.addEventListener("click", newChallenger)
 };
