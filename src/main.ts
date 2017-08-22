bfd2f53ff949b775f13a2189b1f2a14b98f0c434
maxwell burson <maxwellburson@gmail.com>
2017-08-22 13:21:05 -0700

    starting to come together

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,127 @@
 
-function newChallenger() {
-    var challengerName = document.getElementById("challenger-name")
-    if (challengerName == null) {
+function newChallenger(game: Game) {
+    let nameNode = <HTMLInputElement>document.getElementById("challenger-name")
+    if (nameNode == null) {
         console.error("challengerName is empty");
         return;
     }
-    var challengerStat = document.getElementById("challenger-stat")
-    if (challengerStat == null) {
+    let statNode = <HTMLInputElement>document.getElementById("challenger-stat")
+    if (statNode == null) {
         console.error("challengerStat is empty");
         return;
     }
+    let name = nameNode.value;
+    let health = Number(statNode.value);
+
+    game.newChallenger(new Champion(name, {health: health}));
 }
 
 class Game {
     challenger: Champion | null;
     champion: Champion | null;
-    queue: Array<Champion> = [];
+    queue: Champion[] = [];
+    handleNewChallenger: (challenger: Champion) => void;
+    handleNewFight: (queue: Champion[]) => Champion | null;
+    handleTickFight: (chamption: Champion, challenger: Champion) => void;
+    constructor(
+        handleNewChallenger: (challenger: Champion) => void,
+        handleNewFight: (queue: Champion[]) => Champion,
+        handleTickFight: (chamption: Champion, challenger: Champion) => void) {
+            this.handleNewChallenger = handleNewChallenger;
+            this.handleNewFight = handleNewFight;
+            this.handleTickFight = handleTickFight;
+        }
     newChallenger(challenger: Champion) {
+        this.handleNewChallenger(challenger);
         this.queue.push(challenger);
     }
     newFight() {
+        if (this.challenger != null) {
+            console.error(`new fight error, challenger already exists: ${this.challenger}`)
+            return;
+        }
+        let challenger = this.handleNewFight(this.queue)
+        if (challenger == null) {
+            console.error("no challenger picked");
+            return ;
+        }
+        console.log(`new fight: ${challenger}`)
+        if (this.champion != null)
+            this.challenger = challenger;
+        else {
+            console.log("no champion, challener becomes champion");
+            this.champion = challenger;
+        }
 
+    }
+    tickFight() {
+        if (this.challenger == null) {
+            console.error("cannot tick fight, missing challenger");
+            return ;
+        }
+        if (this.champion == null) {
+            console.error("cannot tick fight, missing champion");
+            return ;
+        }
+
+        this.handleTickFight(this.champion, this.challenger); //todo: how does this set stuff to null?
     }
 }
 
 class Champion {
-    name: string = "";
+    name: string;
     status: Status;
     constructor(name: string, status: Status) {
         this.name = name;
         this.status = status
     }
     toString() {
-        var value = `challenger: ${this.name}\n`;
+        var str = `${this.name}\n`;
         for (let key in this.status) {
-            console.log(`key? ${key}`)
+            str += `   ${key}: ${this.status[key]}\n`;
         }
+        return str;
     }
 }
 
-class Status {
+interface Status {
     health: number;
 }
 
 window.onload = function() {
-    // var canvas = document.getElementById("game");
-    // var ctx = canvas.getContext("2d")
-    // ctx.fillStyle = "FF0000";
-    // ctx.fillRect(0, 0, 150, 75);
-    //document.body.innerHTML = greeter("yo")
+    let game = new Game(
+        function(challenger: Champion) {
+            console.log(`new challenger: ${challenger}`);
+        },
+        function(queue: Champion[]) {
+            return queue.pop();
+        },
+        function(champion: Champion, challenger: Champion) {
+            console.log("fight tick");
+        }
+    );
     var newChallengerButton = document.getElementById("new-challenger")
     if (newChallengerButton == null) {
         console.error("no new-challenger button found");
         return;
     }
-    newChallengerButton.addEventListener("click", newChallenger)
+    newChallengerButton.addEventListener("click", function(){
+        newChallenger(game)
+    });
+    var newChallengerButton = document.getElementById("new-fight")
+    if (newChallengerButton == null) {
+        console.error("no new-fight button found");
+        return;
+    }
+    newChallengerButton.addEventListener("click", function(){
+        game.newFight();
+    });
+    var newChallengerButton = document.getElementById("tick-fight")
+    if (newChallengerButton == null) {
+        console.error("no tick-fight button found");
+        return;
+    }
+    newChallengerButton.addEventListener("click", function(){
+        game.tickFight();
+    });
 };
