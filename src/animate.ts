7f39c569789b9609c1ed49ea68348bac17dbc68c
InADyes <liuhao0320@gmail.com>
2017-08-24 15:31:40 -0700

    fix tinny bugs

--- src/animate.ts
+++ src/animate.ts
@@ -16,7 +16,7 @@ if (ctx == null) {
     console.error("why is ctx null, wtf");
     return;
 } 
-ctx.clearRect(0, 0, canvas.height, canvas.width);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 //ctx.font = "30px Arial";
 //ctx.fillText(String(game.counter), 10, 50);
 //game.counter++;
