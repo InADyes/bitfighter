acd5abb644de6805597969ff72b8293b8f1994fd
maxwell burson <maxwellburson@gmail.com>
2017-08-29 18:24:18 -0700

    removed files that should not be tracked

--- game/lib/game/Actor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Actor = (function () {
-    function Actor(ctx, pos) {
-        this.ctx = ctx;
-        this.pos = pos;
-    }
-    return Actor;
-}());
-//# sourceMappingURL=Actor.js.map
