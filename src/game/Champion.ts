3ef8d89b4cd8558402e7c78ac2e01809ba11c2dc
Shawn Hicks <Mithicoron@Shawns-MacBook-Pro.local>
2017-08-29 18:14:22 -0700

    Adding a bunch of stuff

--- src/game/Champion.ts
+++ src/game/Champion.ts
@@ -5,6 +5,8 @@ export class Champion extends Actor{
     private name: string;
     private icon: string;
     private sprite: string;
+    private attCD: number;
+    private dmgChk: boolean;
     private stats: {
             hp: number;
             att: number;
@@ -48,10 +50,20 @@ export class Champion extends Actor{
         this.opponent = opponent;
     }
     public tick(timeDelta: number) {
+        this.attCD = this.attCD + timeDelta;
+        if (this.attCD >= this.stats.attspd){
+            this.toHit();
+            this.attCD = this.attCD - this.stats.attspd;
+        }
+        if (this.dmgChk == true){
+            this.dmgRoll();
+            this.dmgChk = false;
+        }
         this.draw();
         this.healthBar.tick(timeDelta);
     }
     protected draw() {
+        
     }
     public donate(amount: number) {
     }
@@ -60,6 +72,36 @@ export class Champion extends Actor{
     }
     public setPosition(pos: {x: number, y: number}) {
         this.pos = pos;
+    }
+    protected toHit(){
+        let total:      number;
+        let roll:       number;
+        if (this.opponent == null)
+            return;
+        total = this.stats.att + this.opponent.stats.def;
+        roll = Math.ceil(Math.random() * total);
+        if (roll <= this.stats.att)
+            this.dmgChk = true;
+        return;
+    }
+    protected dmgRoll(){
+        let damage:      number;
+        if (this.opponent == null)
+            return;
+        damage = this.stats.dmg - this.opponent.stats.armr;
+        if (damage > 0){
+            this.opponent.stats.hp = this.opponent.stats.hp - damage;
+            this.opponent.healthBar.setHealth(this.opponent.stats.hp);
+        }
+        if (this.opponent.stats.hp <= 0){
+            this.opponent.stats.hp = 0;
+            this.opponent.healthBar.setHealth(0);
+        }
+    }
+    public isDead(){
+        if (this.stats.hp <= 0)
+            return true;
+        return false;
     }
 }
 
