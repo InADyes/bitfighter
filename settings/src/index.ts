1f7e2df5ad3ff18e2ec0da640f24b53f63e8a4df
InADyes <liuhao0320@gmail.com>
2017-08-30 17:23:50 -0700

    merge from master

--- settings/src/index.ts
+++ settings/src/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-/*/// <reference path='game/Game.ts' />
-
-
-=======
->>>>>>> 9d3f596a8ffe78b78a4604441e912f8d6d98ea21
 document.addEventListener("DOMContentLoaded", function(){
     let newDonationButton = document.getElementById("new-donation");
     let nameInputNode = <HTMLInputElement>document.getElementById("donation-name");
@@ -25,37 +19,6 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("donation", JSON.stringify({id: id, name: name, amount: amount, art: art}));
     });
 });
-*/
-document.addEventListener("DOMContentLoaded", function(){
-     let newDonationButton = document.getElementById("new-donation");
-     let nameInputNode = <HTMLInputElement>document.getElementById("donation-name");
-     let idInputNode = <HTMLInputElement>document.getElementById("donation-id");
-     let bitsInputNode = <HTMLInputElement>document.getElementById("donation-bits");
-     let artInputNode = <HTMLInputElement>document.getElementById("donation-art");
-    // let arenaFront = <HTMLCanvasElement>document.getElementById("arena-front");
-    // let arenaBack = <HTMLCanvasElement>document.getElementById("arena-front");
-    if (newDonationButton == null || nameInputNode == null || idInputNode == null || bitsInputNode == null || artInputNode == null)
-        {
-            console.log("initial fail");
-            return;
-        } 
-    newDonationButton.addEventListener("click",function(element){
-        if(nameInputNode.value == null || idInputNode.value == null || bitsInputNode.value == null || artInputNode.value == null)
-            {
-                console.log("missing stats");
-                return;
-            }
-        else
-            {
-                localStorage.setItem("LSDonName", nameInputNode.value);
-                localStorage.setItem("LSDonID", idInputNode.value);
-                localStorage.setItem("LSDonBits", bitsInputNode.value);
-                localStorage.setItem("LSDonArt", artInputNode.value);
-            }        
-    });
-    
-    window.addEventListener('storage', function(element){
-        window.alert("storage changed");
-    })
- });
+
+
  
