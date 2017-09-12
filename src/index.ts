ee9107cb2c8be40c0f4e9b95a17ce4d2f7a6160f
InADyes <liuhao0320@gmail.com>
2017-09-11 17:01:57 -0700

    new branch

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
 import * as Game from './Game';
+import * as fabric from 'fabric';
 
 document.addEventListener("DOMContentLoaded", function(){
     let arenaFront = <HTMLCanvasElement>document.getElementById("arena-front");
     let arenaBack = <HTMLCanvasElement>document.getElementById("arena-front");
+    let arenaWrapper = <HTMLDivElement>document.getElementById("arena-wrapper");
+    let scaleratio = 1;
     if (arenaFront == null || arenaBack == null) {
         console.error("missing DOM hook");
         return;
     }
+    let devicePixelRatio = window.devicePixelRatio || 1;
+    
+    if(arenaWrapper.style.width == null)
+        return;
+    let widthnumber = 300;
+    
+    //let abcde = new fabric.Canvas('arena-front');
+    //fabric.Image.fromURL('dist/images.characters.axe.png', function(oImg){
+    //    abcde.add(oImg);
+   // });
+    
+    let ctx = arenaFront.getContext('2d');
+    
+    window.addEventListener("mouseover", function(){
+        if(arenaWrapper.style.width == null)
+            return;
+        
+        scaleratio = devicePixelRatio*(Number(arenaWrapper.style.width.replace(/[^\d\.\-]/g, '')))/300;
+       
+        arenaFront.width = arenaFront.clientWidth * Math.sqrt(scaleratio);
+        arenaFront.height = arenaFront.clientHeight * Math.sqrt(scaleratio);
+
+        if(ctx == null)
+            return;
+        ctx.scale(scaleratio, scaleratio);
+
+        console.log(String(Number(arenaWrapper.style.width.replace(/[^\d\.\-]/g, ''))));
+        console.log(String(widthnumber));
+        console.log(String(scaleratio));
+        
+    })
+    
+    //console.log(String(scaleratio));
+    
 
     let game = new Game.Game(arenaFront, arenaBack);
     game.seed(100);
@@ -26,4 +63,5 @@ document.addEventListener("DOMContentLoaded", function(){
                 console.error('unidentified storage event');
         }
     });
+    game.donate({id: 1, name: "bob", amount: 100, art:2});
 });
