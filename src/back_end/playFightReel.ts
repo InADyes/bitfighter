da4f9530996f60475c3b61823f51ce0946c61a46
maxwell burson <maxwellburson@gmail.com>
2017-09-15 18:12:19 -0700

    saving progress

--- src/back_end/playFightReel.ts
+++ src/back_end/playFightReel.ts
@@ -1,1 +1,13 @@
 import * as FightReel from '../shared/fightReel';
+import { Status } from '../shared/help';
+
+export function playFightReel(
+    status: Status[],
+    reel: FightReel.Event[],
+    playTo: number
+) {
+    for (let event of reel) {
+
+    }
+    return status;
+}
