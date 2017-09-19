7759871bccc265e085d41af59f762a65b9695159
Tim Jose <timjose3@gmail.com>
2017-09-18 17:42:35 -0700

    ready for the merge lyfe

--- src/front_end/Display.ts
+++ src/front_end/Display.ts
@@ -9,16 +9,105 @@ export class Display {
     }
 }
 
-export class gameState {
-	public canvas:	fabric.Canvas;
-	public img1:	HTMLImageElement;
-	public img2:	HTMLImageElement;
-	public p1:		fabric.Image;
-	public p2:		fabric.Image;
-	
+export class GameState {
+	public	canvas:	fabric.Canvas;
+	private img1:	HTMLImageElement;
+	private img2:	HTMLImageElement;
+	private p1:		fabric.Image;
+	private p2:		fabric.Image;
+	private art = [
+	    "images/characters/axe.png",
+ 		"images/characters/sword.png",
+	   	"images/characters/daggers.png",
+    	"images/characters/champion_alpha.png",
+	]
+
 	constructor() {
-		this.canvas = new fabric.Canvas('arena'); // USE StaticCanvas for noninteractive
-		this.img1 = new Image();
-		this.img2 = new Image();
+		this.canvas =	new fabric.Canvas('arena'); // USE StaticCanvas for noninteractive
+		this.img1 =		new Image();
+		this.img2 =		new Image();
 	}
+
+	public setArt(i: number, player: number) {
+		if (player == 1)
+			this.img1.src = this.art[i];
+		else
+			this.img2.src = this.art[i];
+	}
+
+	public drawPlayers (message: any) {
+        console.log("drawing players");
+        if (this.p1)
+            this.canvas.remove(this.p1);
+        if (this.p2)
+            this.canvas.remove(this.p2);
+        this.setArt(message.characters[0].art, 1)
+        this.p1 = new fabric.Image(this.img1, {
+            left: 30,
+            top: 100,
+        });
+        this.p1.scaleToWidth(200);
+        this.canvas.add(this.p1);
+        this.setArt(message.characters[1].art, 2);
+        this.p2 = new fabric.Image(this.img2, {
+            left: 350,
+            top: 100,
+            flipX: true,
+        });
+        this.p2.scaleToWidth(200);
+        this.canvas.add(this.p2);
+    }
+
+	public p1Attacks () {
+		let g = this;
+	    g.p1.animate('left', '-=20', {
+	        duration: 250,
+	        onChange: g.canvas.renderAll.bind(g.canvas),
+	        easing: fabric.util.ease['easeOutQuad'],
+	        onComplete: function() {
+        		g.p1.animate('left', '+=120', {
+	                duration: 100,
+	                easing: fabric.util.ease['easeInQuint'],
+	                onChange: g.canvas.renderAll.bind(g.canvas),
+	                onComplete: function () {
+	                    g.p1.animate('left', 30, {
+	                        duration: 200,
+	                        onChange: g.canvas.renderAll.bind(g.canvas),
+	                        easing: fabric.util.ease['easeOutQuint'],
+	                	})
+	                }
+                });
+	        }
+	    });
+	}
+	public p2Attacks () {
+		let g = this;
+	    g.p2.animate('left', '+=20', {
+	        duration: 250,
+	        onChange: g.canvas.renderAll.bind(g.canvas),
+	        easing: fabric.util.ease['easeOutQuad'],
+	        onComplete: function () {
+	            g.p2.animate('left', '-=120', {
+	                duration: 100,
+	                easing: fabric.util.ease['easeInQuint'],
+	                onChange: g.canvas.renderAll.bind(g.canvas),
+	                onComplete: function () {
+	                    g.p2.animate('left', 350, {
+	                        duration: 200,
+	                        onChange: g.canvas.renderAll.bind(g.canvas),
+	                        easing: fabric.util.ease['easeOutQuint'],
+	                    })
+	                }
+	            });
+	        }
+	    });
+	}
+}
+
+// until we get taras' art i guess
+export const enum Art {
+	axe,
+	sword,
+	daggers,
+	champion,
 }
--- src/front_end/index.ts
+++ src/front_end/index.ts
@@ -2,25 +2,26 @@ import 'fabric'
 declare let fabric: any;
 import * as Display from './Display';
 import { Message } from '../shared/frontEndMessage';
+import * as Reel from '../shared/displayReel'
 
 
-let player1 = {
-    name: "hao",
-    hitPoints: 1000,
-    art: "images/characters/sword.png",
-    side: 0,
-},
-    player2 = {
-    name: "max",
-    hitPoints: 1000,
-    art: "images/characters/axe.png",
-    side: 1,
-};
 
 
 document.addEventListener("DOMContentLoaded", function(){
-    let gameState = new Display.gameState;
-    eventListeners(gameState);
+    let gameState = new Display.GameState;
+
+    let message = {
+        characters: [{
+                name: "hao",
+                hitPoints: 1000,
+                art: Display.Art.axe,
+            },{
+                name: "max",
+                hitPoints: 1000,
+                art: Display.Art.daggers,
+            }]
+    };
+    eventListeners(gameState, message);
 
     let textOut = <HTMLDivElement><HTMLDivElement>document.getElementById('text-out');
 
@@ -44,33 +45,13 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-function eventListeners(g: Display.gameState) {
-    let draw1 = <HTMLButtonElement>document.getElementById("draw1");
-    draw1.addEventListener("click", function() {
-        console.log("draw player 1");
-        if (g.p1)
-            g.canvas.remove(g.p1);
-        g.img1.src = player1.art;
-        g.p1 = new fabric.Image(g.img1, {
-            left: 30,
-            top: 100,
-        });
-        g.p1.scaleToWidth(200);
-        g.canvas.add(g.p1);
-    });
-    let draw2 = <HTMLButtonElement>document.getElementById("draw2");
-    draw2.addEventListener("click", function() {
-        console.log("draw player 2");
-        if (g.p2)
-            g.canvas.remove(g.p2);
-        g.img2.src = player2.art;
-        g.p2 = new fabric.Image(g.img2, {
-            left: 350,
-            top: 100,
-            flipX: true,
-        });
-        g.p2.scaleToWidth(200);
-        g.canvas.add(g.p2);
+
+
+
+function eventListeners(g: Display.GameState, message: any) {
+    let draw = <HTMLButtonElement>document.getElementById("draw");
+    draw.addEventListener("click", function() {
+         g.drawPlayers(message);
     });
 
     let health = <HTMLButtonElement>document.getElementById("health");
@@ -83,9 +64,9 @@ function eventListeners(g: Display.gameState) {
         let left = <HTMLInputElement>document.getElementById("left");
         console.log((left.checked ? "p1" : "p2") + " attacks");
         if (left.checked)
-            p1Attacks(g);
+            g.p1Attacks();
         else
-            p2Attacks(g);
+            g.p2Attacks();
     });
     let clear = <HTMLDivElement>document.getElementById("clear");
     clear.addEventListener("click", function() {
@@ -97,47 +78,3 @@ function eventListeners(g: Display.gameState) {
     });
 }
 
-function p1Attacks (g: Display.gameState) {
-    g.p1.animate('left', '-=20', {
-        duration: 250,
-        onChange: g.canvas.renderAll.bind(g.canvas),
-        easing: fabric.util.ease['easeOutQuad'],
-        onComplete: function () {
-            g.p1.animate('left', '+=120', {
-                duration: 100,
-                easing: fabric.util.ease['easeInQuint'],
-                onChange: g.canvas.renderAll.bind(g.canvas),
-                onComplete: function () {
-                    g.p1.animate('left', 30, {
-                        duration: 200,
-                        onChange: g.canvas.renderAll.bind(g.canvas),
-                        easing: fabric.util.ease['easeOutQuint'],
-                    })
-                }
-            });
-        }
-    });
-}
-
-function p2Attacks (g: Display.gameState) {
-    g.p2.animate('left', '+=20', {
-        duration: 250,
-        onChange: g.canvas.renderAll.bind(g.canvas),
-        easing: fabric.util.ease['easeOutQuad'],
-        onComplete: function () {
-            g.p2.animate('left', '-=120', {
-                duration: 100,
-                easing: fabric.util.ease['easeInQuint'],
-                onChange: g.canvas.renderAll.bind(g.canvas),
-                onComplete: function () {
-                    g.p2.animate('left', 350, {
-                        duration: 200,
-                        onChange: g.canvas.renderAll.bind(g.canvas),
-                        easing: fabric.util.ease['easeOutQuint'],
-                    })
-                }
-            });
-        }
-    });
-
-}
