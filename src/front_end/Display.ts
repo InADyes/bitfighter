8bda2b2f75b81d6090ca0e657f254931f1aa2bd8
maxwell burson <maxwellburson@gmail.com>
2017-09-20 14:47:29 -0700

    some stuff?

--- src/front_end/Display.ts
+++ src/front_end/Display.ts
@@ -5,10 +5,13 @@ declare let fabric: any;
 import * as Player from './Player';
 
 export class GameState {
-	public canvas:	fabric.Canvas;
-	public reel:	Events.Event[];
-	public player1: Player.Player;
-	public player2: Player.Player;
+	private eventLoopTimeout: NodeJS.Timer | null;
+	private lastTime: number;
+
+	private canvas:	fabric.Canvas;
+	private reel:	Events.Event[];
+	private player1: Player.Player;
+	private player2: Player.Player;
 	//public players: Player[] = []; Eventually do this
 
 
@@ -16,14 +19,43 @@ export class GameState {
 		this.canvas = new fabric.Canvas('arena'); // USE StaticCanvas for noninteractive
 	}
 
-	public displayReel() {
-		let current: Events.Event | undefined;
-		current = this.reel.shift();
-			this.fireEvent(current);
+	private initReel() {
+		if (this.reel.length < 1) {
+			return;
+		}
+
+		this.lastTime = 0;
+		this.eventLoopTimeout = setTimeout(
+			() => {
+				this.fireEvent();
+			},
+			this.reel[0].time
+		);
 	}
 
-	private fireEvent(event: Events.Event) {
+	// private displayReel() {
+	// 	let current = this.reel.shift();
+	// 	if (current === undefined)
+	// 		return; 
+	// 	this.fireEvent(current);
+	// }
+
+	private fireEvent() {
+		let event = this.reel.shift();
+		if (event == undefined) {
+			this.eventLoopTimeout = null;
+			return;
+		}
+
 		console.log(`do this event ${ event }`);
+
+		this.eventLoopTimeout = setTimeout(
+			() => {
+				this.fireEvent();
+			},
+			event.time -  this.lastTime
+		);
+		this.lastTime = event.time;
 	}
 
 	public initPlayers(characters: {name: string, hitPoints: number, art: number}[]) {
