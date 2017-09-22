9be786ff0eb4f317d8be51bf8f60667e8cefbef4
Tim Jose <timjose3@gmail.com>
2017-09-22 13:00:40 -0700

    update folders

---
+++ src/front_end/modules/fireEvent.ts
@@ -0,0 +1,28 @@
+import * as GraphicsEvents from '../../shared/graphicsEvents';
+import { GameState } from './Gamestate'
+
+export function fireEvent(event: GraphicsEvents.Event, gameState: GameState){
+    let char = event.character;
+    switch(event.type) {
+        case GraphicsEvents.EventType.Health:
+            gameState.changeHealth(char, (<GraphicsEvents.Health>event).health)
+            break;
+        case GraphicsEvents.EventType.Attack:
+            console.log(`CHARACTER ${ char } ATTACKS`)
+            gameState.attack(char);
+            break;
+        case GraphicsEvents.EventType.Clear:
+            console.log(`CHARACTER ${ char } DIES`);
+            gameState.slay(char);
+            break;
+        case GraphicsEvents.EventType.Text:
+            console.log(`CHARACTER ${ char } SAYS ${ (<GraphicsEvents.Text>event).text }`);
+            gameState.displayText(char, (<GraphicsEvents.Text>event).text, (<GraphicsEvents.Text>event).color)
+            break;
+        case GraphicsEvents.EventType.Buff:
+            console.log(`BUFF SOMEONE`);
+            break;
+        default:
+            console.error('unidentified event type')
+    }
+}
