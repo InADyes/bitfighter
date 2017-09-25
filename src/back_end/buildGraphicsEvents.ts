c53a608ac834c461fdd3ed14587fa73345b993a5
maxwell burson <maxwellburson@gmail.com>
2017-09-25 10:59:45 -0700

    fixed event order

--- src/back_end/buildGraphicsEvents.ts
+++ src/back_end/buildGraphicsEvents.ts
@@ -2,6 +2,17 @@ import * as GraphicsEvents from '../shared/graphicsEvents';
 import * as FightEvents from '../shared/fightEvents';
 import { otherCharacter } from '../shared/utility';
 
+
+// could be programatically genrated in a cleaner way if we end up with a lot
+// used to determine order of events when the timestamp is the same
+const eventOrder = {
+    [GraphicsEvents.EventType.Attack]: 1,
+    [GraphicsEvents.EventType.Buff]: 1,
+    [GraphicsEvents.EventType.Clear]: 0,
+    [GraphicsEvents.EventType.Health]: 1,
+    [GraphicsEvents.EventType.Text]: 1
+}
+
 export function build(fight: FightEvents.Event[]) {
     let display: GraphicsEvents.Event[] = [];
     
@@ -116,7 +127,10 @@ export function build(fight: FightEvents.Event[]) {
     }
 
     display.sort((a, b) => {
+        if (a.time === b.time)
+            return eventOrder[a.type] - eventOrder[b.type];
         return a.time - b.time;
+        
     });
 
     return display;
