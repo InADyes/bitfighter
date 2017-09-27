6f4f2c42c5ed281b8232c1948b392e406e52464a
maxwell burson <maxwellburson@gmail.com>
2017-09-27 15:01:02 -0700

    oops

--- testbed.js
+++ testbed.js
@@ -3,8 +3,6 @@ const app = express()
 
 app.use(express.static('testbed/'))
 
-//app.use('/settings', express.static('settings/', [{dotfiles: 'allow'}]));
-
 app.listen(3000, function () {
   console.log('listening on port 3000')
 })
