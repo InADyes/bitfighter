4388e48421506a02dee545a7ba4475ef99d5170f
maxwell burson <maxwellburson@gmail.com>
2017-09-28 16:00:42 -0700

    now matching what keith wants more

--- src/front_end/viewer.ts
+++ src/front_end/viewer.ts
@@ -6,12 +6,6 @@ declare function emitGameEvent(slug: string, data: FrontToBackMessage) : void;
 
 let bitFighter: BitFighter;
 
-function recievedViewerGameState(data: BackToFrontMessage) {
-    // This function will get fired everytime there is a new viewer game state received
-    // this will hold game state information relative to the viewer
-    bitFighter.recievedViewerGameState(data);
-}
-
 document.addEventListener('DOMContentLoaded', function(){
     const wrapperDiv = <HTMLDivElement>document.getElementById('bitfighter');
 
