f992cd46eda0c7feeb26baf4c8d9c6e1cad56716
Tim Jose <timjose3@gmail.com>
2017-10-03 12:46:17 -0700

    asd

--- src/front_end/gamestate/Gamestate.ts
+++ src/front_end/gamestate/Gamestate.ts
@@ -39,7 +39,7 @@ export class GameState {
 
 	public newMessage(reel: Events.Event[], characters: {name: string, currentHitPoints: number, maxHitPoints: number, art: number}[], patch?: number) {
 		// Add received message to the queue
-		
+		console.log(performance.now());
 		// Don't do anything if a character is dying or moving over
 		if ((this.player1 && this.player1.isAnimated())
 			|| (this.player2 && this.player2.isAnimated())) {
