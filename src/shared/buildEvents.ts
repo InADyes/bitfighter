1876db4fb0339836995a61012dffcc4bf11a83fd
maxwell burson <maxwellburson@gmail.com>
2017-10-04 11:11:46 -0700

    missing files

---
+++ src/shared/buildEvents.ts
@@ -0,0 +1,88 @@
+import * as FightEvents from './fightEvents';
+import { Event as GraphicsEvent } from './graphicsEvents';
+import { Combatant } from './Combatant';
+import { Status } from '../shared/Status';
+import { applyFightEvents, CombindedEvent } from './applyFightEvents';
+import { otherCharacter  as other} from './utility';
+import { buffs, types as buffTypes } from './buff';
+import * as BuildGraphicsEvents from './buildGraphicsEvents';
+
+export function buildEvents(status: Status[]) {
+    const reel: CombindedEvent[] = [];
+    const newStatus = status.map(s => s.clone());
+    const combatants = newStatus.map((status, index) => new Combatant(
+        status,
+        index,
+        event => reel.push(...applyFightEvents(newStatus, event)),
+        attack => {combatants.filter(c => c != attack.attacker)[0].takeHit(attack);}
+    ));
+
+    if (combatants.length < 2) {
+        console.log('not enough combatants to fight');
+        if (combatants[0] && combatants[0].status.hitPoints <= 0) {
+            reel.push(...applyFightEvents(newStatus, new FightEvents.Death(
+                combatants[0].time,
+                0
+            )));
+        }
+        return {combatants: newStatus, reel};
+    }
+    // ------------- type hack starts here
+
+    if (typeMap[newStatus[0].character] === 0 && typeMap[newStatus[1].character] === 2)
+        newStatus[0].addEffect(10000000, buffs[buffTypes.armorBonus]);
+        
+    if (typeMap[newStatus[0].character] === 1 && typeMap[newStatus[1].character] === 0)
+        newStatus[0].addEffect(10000000, buffs[buffTypes.armorBonus]);
+
+    if (typeMap[newStatus[0].character] === 2 && typeMap[newStatus[1].character] === 1)
+        newStatus[0].addEffect(10000000, buffs[buffTypes.armorBonus]);
+
+    if (typeMap[newStatus[1].character] === 0 && typeMap[newStatus[0].character] === 2)
+        newStatus[1].addEffect(10000000, buffs[buffTypes.armorBonus]);
+
+    if (typeMap[newStatus[1].character] === 1 && typeMap[newStatus[0].character] === 0)
+        newStatus[1].addEffect(10000000, buffs[buffTypes.armorBonus]);
+
+    if (typeMap[newStatus[1].character] === 2 && typeMap[newStatus[0].character] === 1)
+        newStatus[1].addEffect(10000000, buffs[buffTypes.armorBonus]);
+
+
+    // ---------- end here
+    while (newStatus.length >= 2) {
+        if (combatants[0].time <= combatants[1].time)
+            combatants[0].attack();
+        else
+            combatants[1].attack();
+    }
+
+    newStatus.forEach(s => s.clearBuffs());
+    let winner = combatants.filter(c => c.status == newStatus[0])[0];
+    winner.time += 2000;
+    winner.heal(); //todo: maybe should be done in front end
+    reel.push(...applyFightEvents(newStatus, new FightEvents.LevelUp(
+        winner.time,
+        0
+    )));
+
+    return { combatants: newStatus, reel }
+}
+
+const enum types {
+    holy,
+    magic,
+    physical
+}
+
+const typeMap = [
+    types.physical, // Scullery Maid
+    types.physical, // Barkeep
+    types.holy,     // Medium
+    types.magic,    // Minstrel
+    types.magic,    // Mage
+    types.physical, // Rogue
+    types.holy,     // Warpriest
+    types.magic,    // Warlock
+    types.physical, // Swashbuckler
+    types.magic,    // Dragon
+]
---
+++ src/shared/buildGraphicsEvents.ts
@@ -0,0 +1,132 @@
+import { applyFightEvents } from './applyFightEvents';
+import * as GraphicsEvents from './graphicsEvents';
+import * as FightEvents from './fightEvents';
+import { otherCharacter } from './utility';
+import { Status } from './Status';
+
+// could be programatically genrated in a cleaner way if we end up with a lot
+// used to determine order of events when the timestamp is the same
+const eventOrder = {
+    [GraphicsEvents.EventType.Attack]: 1,
+    [GraphicsEvents.EventType.Buff]: 1,
+    [GraphicsEvents.EventType.Clear]: 2,
+    [GraphicsEvents.EventType.Health]: 1,
+    [GraphicsEvents.EventType.Text]: 1
+}
+
+export function sortGraphicsEvents(events: GraphicsEvents.Event[]) {
+    return events.sort((a, b) => {
+        if (a.time === b.time)
+            return eventOrder[a.type] - eventOrder[b.type];
+        return a.time - b.time;
+        
+    });
+}
+
+export function build(event: FightEvents.Event, status: Status[]) {
+    const display: GraphicsEvents.Event[] = [];
+
+    switch (event.type) {
+        case FightEvents.Types.damage:
+            display.push(new GraphicsEvents.Health(
+                event.time,
+                event.character,
+                Math.ceil(status[event.character].hitPoints)
+            ));
+            display.push(new GraphicsEvents.Text(
+                event.time,
+                event.character,
+                String((<FightEvents.Damage>event).amount),
+                'red'
+            ));
+            break;
+        case FightEvents.Types.dodge:
+            display.push(new GraphicsEvents.Text(
+                event.time,
+                event.character,
+                'dodge',
+                'orange'
+            ));
+            break;
+        case FightEvents.Types.healing:
+            display.push(new GraphicsEvents.Health(
+                event.time,
+                event.character,
+                Math.ceil(status[event.character].hitPoints)
+            ));
+            display.push(new GraphicsEvents.Text(
+                event.time,
+                event.character,
+                String((<FightEvents.Healing>event).amount),
+                'green'
+            ));
+            break;
+        case FightEvents.Types.death:
+            display.push(new GraphicsEvents.Clear(
+                event.time,
+                event.character
+            ));
+            break;
+        case FightEvents.Types.crit:
+            display.push(new GraphicsEvents.Text(
+                event.time,
+                event.character,
+                'crit',
+                'red'
+            ));
+            const { buff, debuff } = <FightEvents.Crit>event;
+            if (debuff) {
+                display.push(new GraphicsEvents.Buff(
+                    event.time,
+                    event.character,
+                    debuff.art,
+                    debuff.duration
+                ));
+            }
+            if (buff) {
+                display.push(new GraphicsEvents.Buff(
+                    event.time,
+                    otherCharacter(event.character),
+                    buff.art,
+                    buff.duration
+                ));
+            }
+            break;
+        case FightEvents.Types.donation:
+            const e = (<FightEvents.Donation>event);
+
+            if (e.donationType === FightEvents.DonationType.damage) {
+                display.push(new GraphicsEvents.Text(
+                    event.time,
+                    event.character,
+                    'donation',
+                    'yellow'
+                ));
+            } else {
+                display.push(new GraphicsEvents.Text(
+                    event.time,
+                    event.character,
+                    'donation',
+                    'yellow'
+                ));
+            }
+            break;
+        case FightEvents.Types.attack:
+            display.push(new GraphicsEvents.Attack(
+                event.time - 150,
+                event.character
+            ));
+            break;
+        case FightEvents.Types.levelUp:
+            display.push(new GraphicsEvents.Text(
+                event.time,
+                event.character, //should this always be zero or be characterOther()?
+                'Level Up!',
+                'gold'
+            ));
+            break;
+        default:
+            console.error('unidentified event type');
+    }
+    return display;
+}
