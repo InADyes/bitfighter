68f3b5bc00bb0a3ea00e31274cf3a5a0ed847c0b
maxwell burson <maxwellburson@gmail.com>
2017-10-05 18:08:14 -0700

    quick fix

--- src/shared/interfaces/donation.ts
+++ src/shared/interfaces/donation.ts
@@ -1,6 +1,6 @@
 export interface Donation {
     readonly id: number,
-    readonly name: string,
+    /*readonly*/ name: string,  //todo: make readonly again
     readonly amount: number,
     readonly profileImageURL: string,
     // boss options are used if this fan becomes the bitboss
