d6669ae5e4436bc4983882ccb74f9961aeaba1cb
Keith Underwood <p4nic510@gmail.com>
2017-10-10 11:48:55 -0700

    Attempt to remove queue from FE BF

--- src/front_end/Queue.ts
+++ src/front_end/Queue.ts
@@ -4,7 +4,6 @@ declare function receiveQueue(data: any): void;
 
 export class Queue {
     constructor (
-        private readonly queueDiv: HTMLDivElement,
         private readonly startTimer: (time: number) => void
     ){}
 
@@ -17,10 +16,6 @@ export class Queue {
     }) {
     
         receiveQueue(queue.queue);
-        this.queueDiv.innerText = "NEXT\n";
-        for (let i = 0; i < queue.queue.length; i++) {
-            this.queueDiv.innerText += queue.queue[i].fanDisplayName + ' - ' + queue.queue[i].championTypeName + '\n';
-        }
         if (queue.timer)
             this.startTimer(queue.timer);
     }
