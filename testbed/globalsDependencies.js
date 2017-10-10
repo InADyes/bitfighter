09de6546a53c009793fada49c49371f564aa4d50
maxwell burson <maxwellburson@gmail.com>
2017-10-10 13:43:15 -0700

    added missing file

---
+++ testbed/globalsDependencies.js
@@ -0,0 +1,14 @@
+window.recalcHp = function (damageAmount, newHp, maxHp) {
+};
+window.flip = function (side) {
+};
+window.updateBitBoss = function (bossData) {
+};
+window.receiveQueue = function (data) {
+};
+window.receiveCharList = function (data) {
+    console.log('receive char list', data);
+};
+window.bossTextOut = function (color, text) {
+    console.log('boss text out: ', color, text);
+};
