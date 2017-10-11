1a385e0e252d6d8e18d058b2df04ffc181714e3a
Keith Underwood <p4nic510@gmail.com>
2017-10-10 18:41:08 -0700

    Update scale on load event as well

--- src/front_end/BitFighter.ts
+++ src/front_end/BitFighter.ts
@@ -45,6 +45,9 @@ export class BitFighter {
         window.addEventListener('resize', () => {
             this.updateScale();
         });
+        window.addEventListener('load', () => {
+            this.updateScale();
+        });
         setTimeout(() => {this.emitGameEvent('bitFighter', {requestReel: true})}, 1000);
     }
 
