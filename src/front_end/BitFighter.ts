792fd402ffad93d645c9902be878389027781425
Keith Underwood <p4nic510@gmail.com>
2017-10-10 18:48:21 -0700

    remove window load listener

--- src/front_end/BitFighter.ts
+++ src/front_end/BitFighter.ts
@@ -45,9 +45,6 @@ export class BitFighter {
         window.addEventListener('resize', () => {
             this.updateScale();
         });
-        window.addEventListener('load', () => {
-            this.updateScale();
-        });
         setTimeout(() => {this.emitGameEvent('bitFighter', {requestReel: true})}, 1000);
     }
 
