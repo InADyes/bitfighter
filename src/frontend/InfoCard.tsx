8049a0c70bd8268c9b87af374502c644614ad9ac
maxwell burson <maxwellburson@gmail.com>
2017-10-31 10:48:01 -0700

    cards sorta working again, need to add stats back in still

--- src/frontend/InfoCard.tsx
+++ src/frontend/InfoCard.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
 import { rarityInfo } from '../shared/globals/rarity';
-import { CharacterCard as CharacterCardInfo } from '../shared/interfaces/backToFrontMessage';
+import { CharacterCard } from '../shared/interfaces/backToFrontMessage';
 import { FrontToBackMessage } from '../shared/interfaces/frontToBackMessage';
 
 export default function InfoCard(
     props: {
-        card: CharacterCardInfo;
+        card: CharacterCard;
         onClick?: () => void;
     }
 ) {
@@ -18,11 +18,17 @@ export default function InfoCard(
         classes += ' cheermote';
 
     return <div className={classes} onClick={props.card.selectable ?props.onClick : undefined}>
+        {props.card.selectable ? undefined : <div className="disabledwrap">
+            <div className="disabled"> 
+                <img src="https://s3.amazonaws.com/operaevent-gather/locked.png"/><br/>
+            This card is only available when using the bitboss cheermote
+            </div>
+        </div>}
         <div className="inner">
             <h3 className={props.card.rarity}>{props.card.className}</h3>
             <div className="image" style={{backgroundImage: `url('${ props.card.art }')`}}>
-                    <img src="https://s3.amazonaws.com/operaevent-gather/tier_10000.gif" />
                 {props.card.bitBossCheerMote ? <div className="cheermoteBadge">
+                    <img src="https://s3.amazonaws.com/operaevent-gather/tier_10000.gif" />
                     <p>BitBoss<br/>Cheermote<br/>Exclusive</p>
                 </div> : undefined}
                 <div className="hoverWrap">
@@ -32,7 +38,7 @@ export default function InfoCard(
             <div className="left">
                 <div className="skill">
                     <h4 className={props.card.rarity}>{props.card.buffName}</h4>
-                    <p><img src={ props.card.buffArt }/>{ props.card.skillText }</p>
+                    <p><img src={props.card.buffArt}/>{props.card.skillText}</p>
                     <div className="clear"></div>
                 </div>
                 <div className="rarity">
