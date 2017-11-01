839ebe28d57e9c1e25c13bcbdbd53f57b8e411f5
Tim Jose <timjose3@gmail.com>
2017-11-01 11:18:32 -0700

    Asd

--- src/frontend/Combatant.tsx
+++ src/frontend/Combatant.tsx
@@ -4,13 +4,15 @@ import { FrontendCharacter } from '../shared/interfaces/backToFrontMessage';
 import InfoCard from './InfoCard';
 
 export default function Combatant(
-    props: {combatant: FrontendCharacter}
+    props: {
+        combatant: FrontendCharacter
+    }
 ) {
     const c = props.combatant;
 
     return <div className="combatant">
         <div>
-            <div className="hitPonts">{`${c.currentHitPoints}/${c.maxHitPoints}`}</div>
+            <div className="hitPoints">{`${c.currentHitPoints}/${c.maxHitPoints}`}</div>
             <div className="name">{c.name}</div>
             </div>
         <img src={c.art} alt={c.className}/>
