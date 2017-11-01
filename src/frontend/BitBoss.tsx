fd178e21c33e635cdb7592066b5e77b214ead547
maxwell burson <maxwellburson@gmail.com>
2017-10-31 17:05:37 -0700

    bitboss html partialy in

--- src/frontend/BitBoss.tsx
+++ src/frontend/BitBoss.tsx
@@ -21,6 +21,23 @@ export default function BitBoss(
         width: `${ props.position.scale * 50 }vw`
     }
 
-    return <div id="bitBoss" style={style}>
+    return <div id="mini-boss-wrapper" className="main tl" style={style}>
+        <div className="bar-wrapper">
+            <div className="bar" style={{width: '60.6667%'}}></div>
+            <div className="damage no-transition" style={{width: '0px', right: '39.3333%'}}></div>
+            </div>
+        <div className="row">
+            <div className="col icon">
+                <img className="profile" src={ b.profileImageURL } alt="avatar"/></div>
+            <div className="col info">
+                <div className="info-row">
+                    <div className="names boss data-outline" >{ b.name }</div>
+                    <div className="character data-outline">
+                        <div className="char-name" >{ b.className }</div>
+                        <div className="hp" >{ b.currentHitPoints } / { b.maxHitPoints }</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>;
 }
