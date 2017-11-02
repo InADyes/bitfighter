0579f0bf65f1622c7b41a1268ee64ee31e82b359
Tim Jose <timjose3@gmail.com>
2017-11-02 16:30:06 -0700

    tryna pull

--- src/frontend/ChoiceCards.tsx
+++ src/frontend/ChoiceCards.tsx
@@ -3,40 +3,41 @@ import * as React from 'react';
 import { CharacterCard } from '../shared/interfaces/backToFrontMessage';
 import InfoCard from './InfoCard';
 
-export default function ChoiceCards(
-    props: {
-        cards: CharacterCard[];
-        endTime: number;
-        callBack: (character: number) => void;
-    }
-) {
-    const time = window.performance.now();
+export default class ChoiceCards {
+    constructor(
+        props: {
+            cards: CharacterCard[];
+            endTime: number;
+            callBack: (character: number) => void;
+        }
+    ) {
+        const time = window.performance.now();
 
-    if (props.endTime < time)
-        return null;
+        if (props.endTime < time)
+            return null;
 
-    return (
-    <div>
-        <div id="choiceCards">
-            {props.cards.map((c, i) =>
-                <InfoCard
-                    card={c}
-                    onClick={() => props.callBack(i)}
-                    key={i}
-                    />
-            )}
-        </div>
-        <div className="charSelectTimerWrapper">
-            <div className="charSelectTimerFlipper">
-                <div className="charSelectTimer">
-                    60
-                </div>
-                <div className="charSelectTimerBack">
-                    <img src="../src/images/icons/bitboss.png" id='bitbossEmoji'></img>
+        return (
+        <div>
+            <div id="choiceCards">
+                {props.cards.map((c, i) =>
+                    <InfoCard
+                        card={c}
+                        onClick={() => props.callBack(i)}
+                        key={i}
+                        />
+                )}
+            </div>
+            <div className="charSelectTimerWrapper">
+                <div className="charSelectTimerFlipper">
+                    <div className="charSelectTimer">
+                        60
+                    </div>
+                    <div className="charSelectTimerBack">
+                        <img src="../src/images/icons/bitboss.png" id='bitbossEmoji'></img>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    );
+        );
+    }
 }
-
