f5f71a55ebf40069e5dfddcd5594639946cdaf00
maxwell burson <maxwellburson@gmail.com>
2017-11-02 17:26:38 -0700

    fixed a bunch of junk, ready to start re-adding in reel event stuff

--- src/frontend/Combatant.tsx
+++ src/frontend/Combatant.tsx
@@ -1,27 +1,45 @@
 import * as React from 'react';
 
 import { FrontendCharacter } from '../shared/interfaces/backToFrontMessage';
+import { GraphicsEvent } from '../shared/interfaces/graphicsEvents';
+
 import InfoCard from './InfoCard';
 
-export default function Combatant(
-    props: {
-        combatant: FrontendCharacter,
-        direction: 'left' | 'right',
-        animateToggle: boolean
+interface Props {
+    combatant: FrontendCharacter,
+    direction: 'left' | 'right',
+    reel: GraphicsEvent[]
+}
+
+interface State {
+    combatant: FrontendCharacter
+}
+
+export default class Combatant extends React.Component {
+    public props: Props;
+    public state: State;
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {combatant: props.combatant};
     }
-) {
-    const c = props.combatant;
-    return <div className="combatant">
-                {props.direction === 'left'
-                    ? <Char art={c.art} className={c.className} direction={props.direction} />
-                    : <HpBar currentHitPoints={c.currentHitPoints} maxHitPoints={c.maxHitPoints} name={c.name} />}
-                {props.direction === 'left'
-                    ? <HpBar currentHitPoints={c.currentHitPoints} maxHitPoints={c.maxHitPoints} name={c.name} />
-                    : <Char art={c.art} className={c.className} direction={props.direction}/>}
-                <div className="cardWrap">
-                    <InfoCard card={c.card} noSprite={true}/>
+
+    render() {
+        const c = this.state.combatant;
+        return (
+            <div className="combatant">
+                    {this.props.direction === 'left'
+                        ? <Char art={c.art} className={c.className} direction={this.props.direction} />
+                        : <HpBar currentHitPoints={c.currentHitPoints} maxHitPoints={c.maxHitPoints} name={c.name} />}
+                    {this.props.direction === 'left'
+                        ? <HpBar currentHitPoints={c.currentHitPoints} maxHitPoints={c.maxHitPoints} name={c.name} />
+                        : <Char art={c.art} className={c.className} direction={this.props.direction}/>}
+                    <div className="cardWrap">
+                        <InfoCard card={c.card} noSprite={true}/>
+                        </div>
                 </div>
-            </div>
+        )
+    }
 }
 
 function Char(
