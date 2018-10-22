077ddefac477c6bd6caa29de46928b90bc716121
Thom <thomswann@gmail.com>
2018-10-22 09:03:47 -0700

    update to game width/height

--- src/main.js
+++ src/main.js
@@ -1,63 +1,30 @@
-import "pixi";
-import "p2";
-import Phaser from "phaser";
+import 'pixi';
+import 'p2';
+import Phaser from 'phaser';
 
-import BootState from "./states/Boot";
-import SplashState from "./states/Splash";
-import GameState from "./states/Game";
+import BootState from './states/Boot';
+import SplashState from './states/Splash';
+import GameState from './states/Game';
 
-import config from "./config";
+import config from './config';
 
 class Game extends Phaser.Game {
   constructor() {
-    const docElement = document.documentElement;
-    let height, width;
+    super(
+      window.innerWidth,
+      window.innerHeight,
+      Phaser.CANVAS,
+      'content',
+      null,
+      true
+    );
 
-    if (window.innerWidth < config.gameWidth + 100 || window.innerHeight < config.gameHeight + 100) {
-      height = config.gameHeight;
-      width = config.gameWidth;
-    } else {
-      height = window.innerHeight;
-      width = window.innerWidth;
-    }
-    super(width, height, Phaser.CANVAS, "content", null, true);
+    this.state.add('Boot', BootState, false);
+    this.state.add('Splash', SplashState, false);
+    this.state.add('Game', GameState, false);
 
-    this.state.add("Boot", BootState, false);
-    this.state.add("Splash", SplashState, false);
-    this.state.add("Game", GameState, false);
-
-    // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
-    if (!window.cordova) {
-      this.state.start("Boot");
-    }
+    this.state.start('Boot');
   }
 }
 
 window.game = new Game();
-
-if (window.cordova) {
-  var app = {
-    initialize: function () {
-      document.addEventListener(
-        "deviceready",
-        this.onDeviceReady.bind(this),
-        false
-      );
-    },
-
-    // deviceready Event Handler
-    //
-    onDeviceReady: function () {
-      this.receivedEvent("deviceready");
-
-      // When the device is ready, start Phaser Boot state.
-      window.game.state.start("Boot");
-    },
-
-    receivedEvent: function (id) {
-      console.log("Received Event: " + id);
-    }
-  };
-
-  app.initialize();
-}
